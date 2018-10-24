944821968f65cb84fa6225d7d94f35837a8603b0
Kevin Funderburg <ksfunderburg@icloud.com>
2018-10-23 20:37:00 -0500

    completed assignment 6pt1A

--- Assignment6/Assign06P1A.cpp
+++ Assignment6/Assign06P1A.cpp
@@ -30,7 +30,7 @@ int main()
        newInt;
    int *intArr = 0;
 
-   // SeedRand(); // disabled for reproducible result
+    SeedRand(); // disabled for reproducible result
 
    do
    {
@@ -130,91 +130,9 @@ void DebugShowCase(int whichCase, int totalCasesToDo,
    cout << "array: ";
    ShowArray(caseValues, caseSize);
 }
-////  -2  -1  8  0  4  -6  2
-//// definition for LowIndexMinNeg (to be filled in - part of exercise)
-//int LowIndexMinNeg(const int a[] , int n)
-//{
-//    ShowArray(a,n);
-//    if(n < 1)
-//        return -999;
-//    if(n == 1)
-//    {
-//        if(a[0] < 0)
-//            return 0;
-//        else
-//            return -999 - (n + 1);
-//    }
-//    int min = LowIndexMinNeg(a + 1, n - 1)+1;
-//    int min2 = LowIndexMinNeg(a - 1, n - 1);
-//
-////    cout << "min1 = " << min1 << endl;
-////    cout << "min2 = " << min2 << endl;
-//
-//    cout << "arr[0] < 0 && arr[0]:\t" << a[0] << " <= " << "arr[min]: " << a[min] << endl;
-//    if(a[0] < a[min] && a[0] < 0)
-//    {
-//        return 0;
-//    }
-//    else if(a[0] < a[min2] && a[0] < 0)
-//    {
-//        return 0;
-//    }
-//    else if(a[min2]<0)
-//    {
-//        if(a[min] == a[min2])
-//        {
-//            if(min < min2)
-//                return min;
-//            else
-//                return min2;
-//        }
-//        return min2;
-//    }
-//    else if(a[min] < 0)
-//    {
-//        if(a[min] == a[min2])
-//        {
-//            if(min < min2)
-//                return min;
-//            else
-//                return min2;
-//        }
-//        return min;
-//    }
-//    else
-//    {
-//        return -999;
-//    }
-//}
 
-//int LowIndexMinNeg2(const int a[], int n) {
-//    ShowArray(a, n);
-//    if (n == 0)
-//        return -999;
-//
-////    if (n == 1)
-////    {
-////        if (a[0] < 0)
-////            return 0;
-////        else
-////            return -999;
-////    }
-//    if (n == 1) return 0;
-//
-//    int minIndexTheRest = LowIndexMinNeg(a + 1, n - 1);
-//
-//
-//    cout << "arr[0]: " << a[0] << " : arr[minIndexTheRest + 1]: " << a[minIndexTheRest + 1] << endl;
-//
-//    if (a[0] < 0 && a[0] <= a[minIndexTheRest + 1]) {
-//        return 0;
-//    } else {
-//        return 1 + minIndexTheRest;
-//    }
-//
-//}
+// definition for LowIndexMinNeg (to be filled in - part of exercise)
 int LowIndexMinNeg(const int a[], int n) {
-//    ShowArray(a, n);
     if (n == 0)
     {
         return -999;
@@ -229,9 +147,6 @@ int LowIndexMinNeg(const int a[], int n) {
     else
     {
         int minIndexTheRest = LowIndexMinNeg(a + 1, n - 1);
-
-//        cout << "arr[0]:\t" << a[0] << ":\t\tarr[minIndexTheRest + 1]:\t" << a[minIndexTheRest + 1]
-//             << "\t\tminIndexTheRest:\t" << minIndexTheRest << "\t\t\tn: " << n << endl;
 
         if (minIndexTheRest < 0)
         {
